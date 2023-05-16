1610da3316cd7c6d3ee2b93072ec89150d3dc75d
Noface07 <yuvrajsoni7@gmail.com>
2023-05-16 11:58:16 +0530

    First
    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from 'react'
+// import { Breadcrumb, Layout, Menu, theme } from "antd";
+import Navbar from "./Components/Navbar.js";
 
-function App() {
+// import type { MenuProps } from 'antd';
+// import { Button, Dropdown, Space } from 'antd';
+// import Carousel from "./Components/Carousel";
+import CarouselFadeExample from "./Components/Carousel";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <>
+      <Navbar />
+      <CarouselFadeExample />
+      </> 
+
   );
-}
+};
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+
+    <nav className="shift">
+    <ul>
+      <li><a href="/">HElooooooooooo</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/">About</a></li>
+      <li><a href="/">Downloads</a></li>
+      <li><a href="/">More</a></li>
+      <li><a href="/">Nice staff</a></li>
+    </ul>
+  </nav>
+
+
+  )
+}
